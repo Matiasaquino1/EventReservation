e71b4b666679890bee0b0b16921dfddedc9f8274
Matiasaquino1 <aquinito2092@gmail.com>
2026-01-22 04:08:27 -0300

    feat: add pagination functionality with query parameter handling in event list

--- front/event-reservations-front/src/app/features/events/event-list.component.ts
+++ front/event-reservations-front/src/app/features/events/event-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { EventService } from '../../core/services/event.service';
 import { EventModel } from '../../core/models/event.model';
@@ -28,11 +29,33 @@ export class EventListComponent implements OnInit {
     limit: 10
   };
 
-  constructor(private eventService: EventService) {}
+  constructor(
+  private eventService: EventService,
+  private route: ActivatedRoute,
+  private router: Router
+) {}
 
   ngOnInit(): void {
+  this.route.queryParams.subscribe(params => {
+    this.filters = {
+      location: params['location'] || '',
+      date: params['date'] || '',
+      minTickets: params['minTickets']
+        ? Number(params['minTickets'])
+        : undefined,
+      maxPrice: params['maxPrice']
+        ? Number(params['maxPrice'])
+        : undefined,
+      page: params['page']
+        ? Number(params['page'])
+        : 1,
+      limit: this.filters.limit ?? 10
+    };
+
     this.loadEvents();
+  });
   }
+
 
   loadEvents(): void {
     this.loading = true;
@@ -49,21 +72,48 @@ export class EventListComponent implements OnInit {
   }
 
   onSearch(): void {
-    this.filters.page = 1;
-    this.loadEvents();
+  this.filters.page = 1;
+
+  this.router.navigate([], {
+    queryParams: {
+      location: this.filters.location || null,
+      date: this.filters.date || null,
+      minTickets: this.filters.minTickets || null,
+      maxPrice: this.filters.maxPrice || null,
+      page: this.filters.page
+    },
+    queryParamsHandling: 'merge'
+  });
   }
 
+
   onReset(): void {
-    this.filters = {
-      location: '',
-      date: '',
-      minTickets: undefined,
-      maxPrice: undefined,
-      page: 1,
-      limit: 10
-    };
-    this.loadEvents();
+  this.router.navigate([], {
+    queryParams: {}
+  });
   }
+
+  nextPage(): void {
+  this.router.navigate([], {
+    queryParams: {
+      page: (this.filters.page ?? 1) + 1
+    },
+    queryParamsHandling: 'merge'
+  });
+  }
+
+  prevPage(): void {
+  if ((this.filters.page ?? 1) <= 1) return;
+
+  this.router.navigate([], {
+    queryParams: {
+      page: (this.filters.page ?? 1) - 1
+    },
+    queryParamsHandling: 'merge'
+  });
+  }
+
+
 }
 
 
