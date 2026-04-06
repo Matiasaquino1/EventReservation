c89b19698f9646cfec6a26a2e0ab05c3c009900f
Matiasaquino1 <aquinito2092@gmail.com>
2026-04-06 20:38:08 -0300

    Added QrController whith Endpoint

---
+++ src/EventReservations.Api/Controllers/QrController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+
+namespace EventReservations.Controllers
+{
+    [ApiController]
+    [Route("qr")]
+    public class QrController : ControllerBase
+    {
+        [HttpGet("{reservationId}/qr")]
+        [AllowAnonymous] 
+        public IActionResult GetQr(int reservationId)
+        {
+            var qrContent = $"https://tuapp.com/validate/{reservationId}";
+
+            using var qrGenerator = new QRCodeGenerator();
+            using var qrCodeData = qrGenerator.CreateQrCode(qrContent, QRCodeGenerator.ECCLevel.Q);
+            using var qrCode = new PngByteQRCode(qrCodeData);
+
+            var pngBytes = qrCode.GetGraphic(8);
+
+            return File(pngBytes, "image/png");
+        }
+    }
+}
