3271fd097e488e3d00d1b45b81892814793a3fdc
Matiasaquino1 <aquinito2092@gmail.com>
2026-04-07 04:33:03 -0300

    Fix typos in Dockerfile comments and app routes catch-all redirect

--- front/event-reservations-front/src/app/app.routes.ts
+++ front/event-reservations-front/src/app/app.routes.ts
@@ -60,5 +60,5 @@ export const routes: Routes = [
     ]
   },
   
-  { path: '**', redirectTo: '' } // Catch-all para rutas inexistentess
+  { path: '**', redirectTo: '' } // Catch-all para rutas inexistentes
 ];
